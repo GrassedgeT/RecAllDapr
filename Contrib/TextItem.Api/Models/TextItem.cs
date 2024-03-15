5acfe7a0b570dd7b69c9767bb61b731a3c3356e5
qianniancao <grassedge@outlook.com>
2024-03-15 20:21:37 +0800

    Add project files.

---
+++ Contrib/TextItem.Api/Models/TextItem.cs
@@ -0,0 +1,11 @@
+﻿namespace RecAll.Contrib.TextItem.Api.Models;
+
+public class TextItem
+{
+    public int Id { get; set; }
+    public string? ItemId { get; set; }
+    public string Content { get; set; }
+    public string UserIdentityGuid { get; set; }
+    public bool IsDeleted { get; set; }
+}
+
---
+++ Contrib/TextItem.Api/Services/TextListContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RecAll.Contrib.TextItem.Api.Services
+{
+    public class TextListContext : DbContext
+    {
+        public DbSet<Models.TextItem> TextItems { get; set; }
+        public TextListContext(DbContextOptions<TextListContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TextItemConfiguration());
+        }
+    }
+
+    public class TextItemConfiguration : IEntityTypeConfiguration<Models.TextItem>
+    {
+        public void Configure(EntityTypeBuilder<Models.TextItem> builder)
+        {
+            builder.ToTable("textitems");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).UseHiLo("textitem_hilo");
+
+            builder.Property(p => p.ItemId).IsRequired(false);
+            builder.HasIndex(p => p.ItemId).IsUnique();
+
+            builder.Property(p => p.Content).IsRequired();
+
+            builder.Property(p => p.UserIdentityGuid).IsRequired();
+            builder.HasIndex(p => p.UserIdentityGuid).IsUnique(false);
+
+            builder.Property(p => p.IsDeleted).IsRequired();
+        }
+    }
+
+    public class  TextListContextDesignFactory : IDesignTimeDbContextFactory<TextListContext>
+    {
+        public TextListContext CreateDbContext(string[] args) =>
+            new(new DbContextOptionsBuilder<TextListContext>()
+            .UseSqlServer(
+                "Server=.;Initial Catalog=RecAll.TextListDb;Integrated Security=true")
+            .Options);
+    }
+}
+
