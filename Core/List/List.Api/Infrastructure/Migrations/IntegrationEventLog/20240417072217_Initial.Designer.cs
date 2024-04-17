0028bd0f1e0288b518ebc1df2df460054ceb8be8
GrassedgeT <grassedge@outlook.com>
2024-04-17 15:24:32 +0800

    intergrateEventlog数据库

--- Core/List/List.Api/Infrastructure/Migrations/IntegrationEventLog/20240417072217_Initial.Designer.cs
+++ Core/List/List.Api/Infrastructure/Migrations/IntegrationEventLog/20240417072217_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecAll.Core.List.Infrastructure;
 
@@ -10,9 +11,11 @@ using RecAll.Core.List.Infrastructure;
 namespace RecAll.Core.List.Infrastructure.Migrations
 {
     [DbContext(typeof(ListContext))]
-    partial class ListContextModelSnapshot : ModelSnapshot
+    [Migration("20240417072217_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
